613db313dabe716a5f5058811cf73ffa09d1c7d9
Nikhil Jose <nikhiljose12345@gmail.com>
2022-10-26 15:31:52 +0530

    fix: mod 0 and timer reset

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 // Iteration 2: Generating two random numbers (0 to 100) and displaying the same in the game.html
 // Iteration 3: Creating variables required to make the game functional
 // Iteration 4: Creating a variable for number 3 and a variable for storing the html element with the Id "number3"
-let num1,num2,num3,operator,seconds;
+let num1,num2,num3,operator;
+let seconds,timer;
 let $num1 = document.getElementById("number1");
 let $num2 = document.getElementById("number2");
 let $num3 = document.getElementById("number3");
@@ -12,10 +13,8 @@ setRandom();
 
 // Iteration 5: Creating a randomise function to make the game functional
 function setRandom() {
-    seconds = 20;
-    document.getElementById("timer").innerText = seconds;
-    let n1 = Math.floor(Math.random() * 100);
-    let n2 = Math.floor(Math.random() * 100);
+    let n1 = Math.round(Math.random() * 100);
+    let n2 = Math.round(Math.random() * 100);
     if(n1>n2){
         num1=n1;
         num2=n2;
@@ -28,8 +27,12 @@ function setRandom() {
     $num2.innerText = num2;
     operator = operatorList[Math.floor(Math.random() * operatorList.length)];
     num3=Math.floor(eval(`${num1} ${operator} ${num2}`))
+    if(isNaN(num3)){
+        setRandom();
+    }
     console.log({num1,num2,num3,operator})
     $num3.innerText = num3;
+    resetTimer()
 }
 
 // Iteration 6: Making the Operators (button) functional
@@ -64,14 +67,25 @@ document.getElementById("buttons").onclick = (e) => {
 };
 
 // Iteration 7: Making Timer functional
-let countDown = setInterval(() => {
+function setTimer(){
+    seconds=20
     document.getElementById("timer").innerText = seconds;
-    if (seconds > 0) {
-        seconds--;
-    } else {
-        gameOver();
-    }
-}, 1000);
+    seconds--
+    timer=setInterval(() => {
+        document.getElementById("timer").innerText = seconds;
+        if (seconds > 0) {
+            seconds--;
+        } else {
+            gameOver();
+        }
+    }, 1000);
+}
+
+function resetTimer(){
+    clearInterval(timer);
+    setTimer();
+}
+
 
 function gameOver() {
     sessionStorage.setItem("score", score);
